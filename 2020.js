34bb81476a03141b90c712eb2b848a98b654129a
Dean Panayotov <deanpanayotov@gmail.com>
2015-01-19 04:08:56 +0200

    Simple alert with days.

--- 2020.js
+++ 2020.js
@@ -2,14 +2,9 @@
  * Created by Dean Panayotov Local on 19.1.2015 г..
  */
 
-function parseDate(str) {
-    var mdy = str.split('/')
-    return new Date(mdy[2], mdy[0]-1, mdy[1]);
-}
-
 function daydiff(first, second) {
     return (second-first)/(1000*60*60*24);
 }
 
-alert(daydiff(parseDate($('#first').val()), parseDate($('#second').val())));
+alert(daydiff(new Date(), new Date(2020,1,1,0,0,0)));
 
