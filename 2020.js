50824e6e6addfcae4a2e27523a92360b5f9eaf0c
Dean Panayotov <deanpanayotov@gmail.com>
2015-01-19 04:32:15 +0200

    First workin version.

--- 2020.js
+++ 2020.js
@@ -2,9 +2,31 @@
  * Created by Dean Panayotov Local on 19.1.2015 г..
  */
 
-function daydiff(first, second) {
-    return (second-first)/(1000*60*60*24);
-}
+var YEAR = 365.25 //days
+var MONTH = 30.416; //days
+var WEEK = 7; //days
 
-alert(daydiff(new Date(), new Date(2020,1,1,0,0,0)));
+var seconds = document.getElementById("seconds");
+var minutes = document.getElementById("minutes");
+var hours = document.getElementById("hours");
+var days = document.getElementById("days");
+var weeks = document.getElementById("weeks");
+var months = document.getElementById("months");
+var years = document.getElementById("years");
 
+var timer = function(){
+    var accum = (new Date(2020,1,1,0,0,0)) - (new Date());
+    accum /= 1000;
+    seconds.innerHTML = accum;
+    accum /= 60;
+    minutes.innerHTML = accum;
+    accum /= 60;
+    hours.innerHTML = accum;
+    accum /=24;
+    days.innerHTML = accum;
+    weeks.innerHTML = accum/WEEK;
+    months.innerHTML = accum/MONTH;
+    years.innerHTML = accum/YEAR;
+};
+
+window.setInterval("timer()", 1000);
